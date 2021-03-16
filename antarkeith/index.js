91d6c13529823972a78fceec00a31741809826f3
Michaela <68235962+mnapearson@users.noreply.github.com>
2021-03-16 15:07:57 +0100

    Add files via upload

---
+++ antarkeith/index.js
@@ -0,0 +1,19 @@
+window.onscroll = function () {
+  myFunction();
+};
+
+var navbar = document.getElementById("nav");
+var section = document.getElementById("bio");
+var sticky = section.offsetTop - 100;
+var typewriter = document.getElementById("animation");
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("nav");
+    navbar.classList.remove("nav-hidden");
+    typewriter.classList.add("animation-on");
+  } else {
+    navbar.classList.add("nav-hidden");
+    navbar.classList.remove("nav");
+  }
+}
